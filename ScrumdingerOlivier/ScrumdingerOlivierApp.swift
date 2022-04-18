b49bd033f3a3efd5fcd9efc86437763857bfcdca
Olivier Van hamme <nicefiction@gmail.com>
2022-04-18 17:24:12 +0200

    Initial Commit

---
+++ ScrumdingerOlivier/ScrumdingerOlivierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerOlivierApp.swift
+//  ScrumdingerOlivier
+//
+//  Created by Olivier Van hamme on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerOlivierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
